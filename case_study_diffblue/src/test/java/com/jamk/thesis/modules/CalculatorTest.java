75e556a7a9684b3e9cdbba5e17b502de99c28c02
Sajad Daghigh <sajad.daghigh@gmail.com>
2025-05-02 01:08:26 +0300

    diffblue generates tests

--- case_study_diffblue/src/test/java/com/jamk/thesis/modules/CalculatorTest.java
+++ case_study_diffblue/src/test/java/com/jamk/thesis/modules/CalculatorTest.java
@@ -1,5 +1,6 @@
 package com.jamk.thesis.modules;
 
+import com.diffblue.cover.annotations.MethodsUnderTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -827,7 +828,6 @@ class CalculatorTest {
     @Test
     @DisplayName("Test factorial(int); when one; then return one")
     @Tag("MaintainedByDiffblue")
-    @MethodsUnderTest({"int Calculator.factorial(int)"})
     void testFactorial_whenOne_thenReturnOne() {
         // Arrange, Act and Assert
         assertEquals(1, (new Calculator()).factorial(1));
@@ -845,7 +845,6 @@ class CalculatorTest {
     @Test
     @DisplayName("Test factorial(int); when two; then return two")
     @Tag("MaintainedByDiffblue")
-    @MethodsUnderTest({"int Calculator.factorial(int)"})
     void testFactorial_whenTwo_thenReturnTwo() {
         // Arrange, Act and Assert
         assertEquals(2, (new Calculator()).factorial(2));
@@ -863,9 +862,116 @@ class CalculatorTest {
     @Test
     @DisplayName("Test factorial(int); when zero; then return one")
     @Tag("MaintainedByDiffblue")
-    @MethodsUnderTest({"int Calculator.factorial(int)"})
     void testFactorial_whenZero_thenReturnOne() {
         // Arrange, Act and Assert
         assertEquals(1, (new Calculator()).factorial(0));
+    }
+
+    /**
+     * Test {@link Calculator#gcd(int, int)}.
+     * <ul>
+     *   <li>When minus one.</li>
+     *   <li>Then return one.</li>
+     * </ul>
+     * <p>
+     * Method under test: {@link Calculator#gcd(int, int)}
+     */
+    @Test
+    @DisplayName("Test gcd(int, int); when minus one; then return one")
+    @Tag("MaintainedByDiffblue")
+    @MethodsUnderTest({"int Calculator.gcd(int, int)"})
+    void testGcd_whenMinusOne_thenReturnOne() {
+        // Arrange, Act and Assert
+        assertEquals(1, (new Calculator()).gcd(-1, 3));
+    }
+
+    /**
+     * Test {@link Calculator#gcd(int, int)}.
+     * <ul>
+     *   <li>When one.</li>
+     *   <li>Then return one.</li>
+     * </ul>
+     * <p>
+     * Method under test: {@link Calculator#gcd(int, int)}
+     */
+    @Test
+    @DisplayName("Test gcd(int, int); when one; then return one")
+    @Tag("MaintainedByDiffblue")
+    @MethodsUnderTest({"int Calculator.gcd(int, int)"})
+    void testGcd_whenOne_thenReturnOne() {
+        // Arrange, Act and Assert
+        assertEquals(1, (new Calculator()).gcd(1, 3));
+    }
+
+    /**
+     * Test {@link Calculator#gcd(int, int)}.
+     * <ul>
+     *   <li>When three.</li>
+     *   <li>Then return three.</li>
+     * </ul>
+     * <p>
+     * Method under test: {@link Calculator#gcd(int, int)}
+     */
+    @Test
+    @DisplayName("Test gcd(int, int); when three; then return three")
+    @Tag("MaintainedByDiffblue")
+    @MethodsUnderTest({"int Calculator.gcd(int, int)"})
+    void testGcd_whenThree_thenReturnThree() {
+        // Arrange, Act and Assert
+        assertEquals(3, (new Calculator()).gcd(3, 3));
+    }
+
+    /**
+     * Test {@link Calculator#gcd(int, int)}.
+     * <ul>
+     *   <li>When zero.</li>
+     *   <li>Then return three.</li>
+     * </ul>
+     * <p>
+     * Method under test: {@link Calculator#gcd(int, int)}
+     */
+    @Test
+    @DisplayName("Test gcd(int, int); when zero; then return three")
+    @Tag("MaintainedByDiffblue")
+    @MethodsUnderTest({"int Calculator.gcd(int, int)"})
+    void testGcd_whenZero_thenReturnThree() {
+        // Arrange, Act and Assert
+        assertEquals(3, (new Calculator()).gcd(0, 3));
+    }
+
+    /**
+     * Test {@link Calculator#permutation(int, int)}.
+     * <ul>
+     *   <li>When three.</li>
+     *   <li>Then return six.</li>
+     * </ul>
+     * <p>
+     * Method under test: {@link Calculator#permutation(int, int)}
+     */
+    @Test
+    @DisplayName("Test permutation(int, int); when three; then return six")
+    @Tag("MaintainedByDiffblue")
+    @MethodsUnderTest({"long Calculator.permutation(int, int)"})
+    void testPermutation_whenThree_thenReturnSix() {
+        // Arrange, Act and Assert
+        assertEquals(6L, (new Calculator()).permutation(3, 3));
+    }
+
+    /**
+     * Test {@link Calculator#permutation(int, int)}.
+     * <ul>
+     *   <li>When zero.</li>
+     *   <li>Then return one.</li>
+     * </ul>
+     * <p>
+     * Method under test: {@link Calculator#permutation(int, int)}
+     */
+    @Test
+    @DisplayName("Test permutation(int, int); when zero; then return one")
+    @Tag("MaintainedByDiffblue")
+    @MethodsUnderTest({"long Calculator.permutation(int, int)"})
+    void testPermutation_whenZero_thenReturnOne() {
+        // Arrange, Act and Assert
+        assertEquals(1L, (new Calculator()).permutation(0, 0));
     }
 }
